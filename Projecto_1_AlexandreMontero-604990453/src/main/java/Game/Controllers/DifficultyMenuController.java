9c2cce70f8673dafb34a3004487fb3a017da06bf
AlexandreMontero <alexandre.montero.espinosa@est.una.ac.cr>
2025-04-01 20:32:35 -0600

    Modification of the difficulty menu, adding the validation of the selected difficulty and unchecking the other to avoid errors

--- Projecto_1_AlexandreMontero-604990453/src/main/java/Game/Controllers/DifficultyMenuController.java
+++ Projecto_1_AlexandreMontero-604990453/src/main/java/Game/Controllers/DifficultyMenuController.java
@@ -1,9 +1,12 @@
 package Game.Controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,8 +15,8 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class DifficultyMenuController {
-    
+public class DifficultyMenuController implements Initializable {
+
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -32,31 +35,53 @@ public class DifficultyMenuController {
     @FXML
     private Label lbDifficultySelection;
 
-    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        EasyMode.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                uncheckOther(EasyMode);
+            }
+        });
+        
+        NormalMode.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                uncheckOther(NormalMode);
+            }
+        });
+        
+        HardMode.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                uncheckOther(HardMode);
+            }
+        });
+    }
+
     @FXML
-    public void switchToModeMenu (ActionEvent event) throws IOException {
+    public void switchToModeMenu(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/Fxml/modemenu.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
+
     @FXML
     public void switchToShips(ActionEvent event) throws IOException {
         String selectedDifficulty = "";
 
         if (EasyMode.isSelected()) {
             selectedDifficulty = "Easy";
-        } else if (NormalMode.isSelected()) {
+        } 
+        if (NormalMode.isSelected()) {
             selectedDifficulty = "Normal";
-        } else if (HardMode.isSelected()) {
+        } 
+        if (HardMode.isSelected()) {
             selectedDifficulty = "Hard";
         }
 
         if (selectedDifficulty.isEmpty()) {
             lbDifficultySelection.setText("¡Por favor, seleccione una dificultad!");
-            return; 
+            return;
         }
 
         String fxmlFile = "";
@@ -74,9 +99,23 @@ public class DifficultyMenuController {
         }
 
         root = FXMLLoader.load(getClass().getResource(fxmlFile));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void uncheckOther(CheckBox selected) {
+        if (selected != EasyMode) {
+            EasyMode.setSelected(false);
+        }
+        
+        if (selected != NormalMode) {
+            NormalMode.setSelected(false);
+        }
+        
+        if (selected != HardMode) {
+            HardMode.setSelected(false);
+        }
     }
 }
